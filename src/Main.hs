f8093d1ab549628d233d53a150d4ed2e2a39763d
Alex Babkin <ababkin@gmail.com>
2014-04-05 15:43:37 -0400

    add notification parsing conduit

--- src/Main.hs
+++ src/Main.hs
@@ -5,33 +5,47 @@ module Main where
 import Control.Monad.Trans
 import Data.Conduit
 import Data.Conduit.Network.UDP
-import qualified Data.ByteString.Char8 as BS
+import Data.Aeson
+import Control.Monad.IO.Class
 
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Conduit.Attoparsec as CA
+
+import Notification
 
 source = do
   socket <- liftIO $ bindPort 5555 HostAny
   sourceSocket socket 1024
-    
-conduit = do
-  liftIO $ putStrLn "conduit calling await"
-  mx <- await
-  case mx of
-    Nothing -> liftIO $ putStrLn "Nothing left, exiting"
-    Just x -> do
-      liftIO $ putStrLn $ "conduit yielding"
-      yield x
-      conduit
-            
-sink = do
-  liftIO $ putStrLn $ "sink: waiting for messages..."
-  maybeMsg <- await
-  case maybeMsg of
-    Nothing -> liftIO $ putStrLn "sink: Nothing from upstream, exiting"
+
+extractData :: Conduit Message IO BS.ByteString
+extractData = do
+  maybeMessage <- await
+  case maybeMessage of
+    Nothing -> liftIO $ putStrLn "No data left to extract, exiting"
     Just msg -> do
-      liftIO $ putStr $ BS.unpack $ msgData msg
+      yield $ msgData msg
+      extractData
+
+parseNotification :: Conduit BS.ByteString IO Notification
+parseNotification = do
+  CA.conduitParserEither json =$= awaitForever go
+  where
+    go (Left s) = error $ show s
+    go (Right (_, j)) = do
+      case fromJSON j :: Result Notification of
+        Success notification  -> yield notification
+        Error s               -> error s
+
+sink = do
+  liftIO $ putStrLn $ "sink: waiting for notifications..."
+  maybeNotification <- await
+  case maybeNotification of
+    Nothing -> liftIO $ putStrLn "sink: Nothing from upstream, exiting"
+    Just notification -> do
+      liftIO $ putStr $ show notification
       sink
             
-main = source $$ conduit =$ sink
+main = source $$ extractData =$= parseNotification =$ sink
   
 
 
---
+++ src/Notification.hs
@@ -0,0 +1,110 @@
+{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE TemplateHaskell #-}
+
+module Notification where
+
+import Control.Applicative
+import Data.Aeson
+import GHC.Generics
+import Control.Lens
+
+import qualified Data.Text as T
+
+data EventType = 
+    ControllerProcessStart
+  | ControllerProcessFinish
+  | Sql
+  | RenderTemplate
+  | RenderPartial
+  | Unknown
+  deriving Show
+
+data PayloadType = 
+    ControllerPayloadType
+  | SqlPayloadType
+  | RenderPayloadType
+  | UnknownPayloadType
+
+data Payload = 
+  RenderPayload{
+    _identifier  :: T.Text
+  }
+  |
+  SqlPayload{
+    _sql         :: T.Text
+  }
+  |
+  ControllerPayload{
+    _controller  :: T.Text
+  , _action      :: T.Text
+  , _params      :: Object
+  , _format      :: T.Text
+  , _method      :: T.Text
+  , _path        :: T.Text
+  , _status      :: Maybe Int
+  } 
+  |
+  UnknownPayload
+  deriving (Generic, Show)
+
+instance ToJSON Payload
+
+
+data Notification = Notification{
+    _sourceType :: T.Text
+  , _eventType  :: EventType
+  , _payload    :: Payload
+  , _timestamp  :: T.Text
+  , _start      :: Int
+  , _duration   :: Int
+  } deriving (Show)
+
+makeLenses ''Payload
+makeLenses ''Notification
+
+instance FromJSON Notification where
+  parseJSON (Object v) = do
+    eventType       <- getEventType <$> v .: "event_type"
+    payload         <- v .: "payload"
+    specificPayload <- case getPayloadType eventType of
+      RenderPayloadType     -> RenderPayload      <$> payload .: "identifier"
+      SqlPayloadType        -> SqlPayload         <$> payload .: "sql"
+      ControllerPayloadType -> ControllerPayload  <$> payload .: "controller"
+                                                  <*> payload .: "action"
+                                                  <*> payload .: "params"
+                                                  <*> payload .: "format"
+                                                  <*> payload .: "method"
+                                                  <*> payload .: "path"
+                                                  <*> payload .:? "status"
+      _ -> return UnknownPayload
+
+    sourceType  <- v .: "source_type"
+    timeStamp   <- v .: "timestamp"
+    start       <- v .: "start_milliseconds"
+    duration    <- v .: "duration"
+
+    return $ Notification sourceType eventType specificPayload timeStamp start duration
+
+    where
+      getPayloadType :: EventType -> PayloadType
+      getPayloadType et = case et of
+        ControllerProcessStart  -> ControllerPayloadType
+        ControllerProcessFinish -> ControllerPayloadType
+        Sql                     -> SqlPayloadType
+        RenderTemplate          -> RenderPayloadType
+        RenderPartial           -> RenderPayloadType
+        Unknown                 -> UnknownPayloadType
+
+      getEventType :: T.Text -> EventType
+      getEventType ett = case ett of
+        "start_processing.action_controller"  -> ControllerProcessStart
+        "process_action.action_controller"    -> ControllerProcessFinish
+        "sql.active_record"                   -> Sql
+        "!render_template.action_view"        -> RenderTemplate
+        "render_template.action_view"         -> RenderTemplate
+        "render_partial.action_view"          -> RenderPartial
+        _                                     -> Unknown
+      
+
+
+
