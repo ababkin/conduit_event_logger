2ab9488fc78ca5951711928941b18df17196bd6f
Alex Babkin <ababkin@gmail.com>
2014-04-05 14:50:33 -0400

    barebones udp -> print conduit is working

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
-main :: IO ()
-main = do
-  putStrLn "hello"
+import Control.Monad.Trans
+import Data.Conduit
+import Data.Conduit.Network.UDP
+import qualified Data.ByteString.Char8 as BS
+
+
+source = do
+  socket <- liftIO $ bindPort 5555 HostAny
+  sourceSocket socket 1024
+    
+conduit = do
+  liftIO $ putStrLn "conduit calling await"
+  mx <- await
+  case mx of
+    Nothing -> liftIO $ putStrLn "Nothing left, exiting"
+    Just x -> do
+      liftIO $ putStrLn $ "conduit yielding"
+      yield x
+      conduit
+            
+sink = do
+  liftIO $ putStrLn $ "sink: waiting for messages..."
+  maybeMsg <- await
+  case maybeMsg of
+    Nothing -> liftIO $ putStrLn "sink: Nothing from upstream, exiting"
+    Just msg -> do
+      liftIO $ putStr $ BS.unpack $ msgData msg
+      sink
+            
+main = source $$ conduit =$ sink
+  
+
+
+
